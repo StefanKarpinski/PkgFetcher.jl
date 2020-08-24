dff8a39b143add998ea540f413050c8f182f924e
Stefan Karpinski <stefan@karpinski.org>
2020-08-24 13:58:21 -0400

    wip

--- src/PkgFetcher.jl
+++ src/PkgFetcher.jl
@@ -7,9 +7,9 @@ using Downloader.Curl: Multi
 
 struct Resource
     hash::String
-    dest::String
-    # post::Function
+    path::String
     # TODO: add artifacts after package download
+    # post::Function
 end
 
 Resource(hash::AbstractString) = Resource(hash, tempname())
@@ -38,14 +38,14 @@ function fetch(curl::Multi, resources::Resources)
     @sync for (resource, sources) in resources
         @async for source in sources
             fetch(curl, resource, source) || continue
-            if !isfile(resource.dest)
+            if !isfile(resource.path)
                 @error "no download found" resource source
                 continue
             end
-            hash = Tar.tree_hash(`gzcat $(resource.dest)`)
+            hash = Tar.tree_hash(`gzcat $(resource.path)`)
             if hash != resource.hash
                 @warn "hash mismatch" resource source hash
-                rm(resource.dest, force=true)
+                rm(resource.path, force=true)
                 continue
             end
             break # success!
@@ -54,22 +54,7 @@ function fetch(curl::Multi, resources::Resources)
 end
 
 function fetch(curl::Multi, resource::Resource, source::Tarball)
-    io = open(resource.dest, write=true)
-    try
-        # TODO: inject various Pkg headers
-        request = Request(io, source.url, Pair{String,String}[])
-        response = Downloader.get(request, curl) # TODO: hook up progress
-        if response.status != 200
-            @warn "tarball download failed" source.url response.status
-            return false
-        end
-    catch error
-        @warn "tarball download error" source.url error
-        close(io)
-        rm(resource.dest, force=true)
-        return false
-    end
-    return true
+    download(curl, source.url, resource.path)
 end
 
 function fetch(curl::Multi, resource::Resource, source::Patch)
@@ -78,10 +63,38 @@ function fetch(curl::Multi, resource::Resource, source::Patch)
     if isfile(skeleton)
         Tar.create(source.old, old_tar, skeleton=skeleton)
     else
-        # no skeleton: assume everything is in the tarball
+        # no skeleton: assume everything goes in the tarball
+        # might fail or give wrong data, but we'll catch it
         Tar.create(source.old, old_tar)
     end
+    patch = tempname()
+    download(curl, source.url, patch) || return false
+    output = pipeline(`gzip`, resource.path)
+    try bspatch(old_tar, output, patch)
+    catch error
+        @warn "could not apply patch" error
+        return false
+    end
+    return true
+end
 
+function download(curl::Multi, url::String, path::AbstractString)
+    io = open(path, write=true)
+    try
+        # TODO: inject various Pkg headers
+        request = Request(io, url, Pair{String,String}[])
+        response = Downloader.get(request, curl) # TODO: hook up progress
+        if response.status != 200
+            @warn "download failed" url response.status
+            return false
+        end
+    catch error
+        @warn "download error" url error
+        close(io)
+        rm(path, force=true)
+        return false
+    end
+    return true
 end
 
 end # module
